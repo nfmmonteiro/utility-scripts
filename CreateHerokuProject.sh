14ac26491df036dd9a92a27c3893c25d1acf28f9
Nuno Monteiro <nfmmonteiro@gmail.com>
2015-02-25 23:28:01 +0000

    Adds shell script to create an Express 4 and deploy it to Heroku

---
+++ CreateHerokuProject.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+echo "Creating the Express App: $1"
+express $1
+cd $1
+
+echo "Creating $1 on Heroku"
+heroku create $1
+git init
+heroku git:remote -a $1
+git add .
+git commit -am "First commit. Adds a Express 4 skeleton app."
+
+echo "Creating shell script to run $1 locally"
+touch run_locally.sh
+chmod +x run_locally.sh
+echo "npm start" > run_locally.sh
+
+echo "Creating README.md file"
+touch README.md
+
+echo "Setting up git repository"
+touch .gitignore
+echo "node_modules" > .gitignore
+
+git add .
+git commit -am "Adds README and Git ignore files."
+git push heroku master
+
+heroku open
+
+echo "Done"
