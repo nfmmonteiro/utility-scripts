8116192a8c87661f8d81aec1951204c4d68b9753
Nuno Monteiro <nfmmonteiro@gmail.com>
2015-03-01 01:33:50 +0000

    Adds more files to .gitignore

--- CreateHerokuProject.sh
+++ CreateHerokuProject.sh
@@ -4,7 +4,7 @@ echo "Creating the Express App: $1"
 express $1
 cd $1
 
-echo "Creating $1 on Heroku"
+echo "Creating $1 on Heroku and setting up Git"
 heroku create $1
 git init
 heroku git:remote -a $1
@@ -19,9 +19,10 @@ echo "npm start" > run_locally.sh
 echo "Creating README.md file"
 touch README.md
 
-echo "Setting up git repository"
 touch .gitignore
 echo "node_modules" > .gitignore
+echo ".DS_Store" >> .gitignore
+echo "*.log" >> .gitignore
 
 git add .
 git commit -am "Adds README and Git ignore files."
